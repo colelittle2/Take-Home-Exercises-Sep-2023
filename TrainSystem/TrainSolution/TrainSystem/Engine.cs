1881d20803244ff36024425b4f7f63e514e7a3bc
Cole Littlechilds <clittlechilds1@nait.ca>
2023-10-03 08:47:22 -0600

    added more to the assignment

--- TrainSystem/TrainSolution/TrainSystem/Engine.cs
+++ TrainSystem/TrainSolution/TrainSystem/Engine.cs
@@ -4,6 +4,11 @@ namespace TrainSystem
 {
     public class Engine
     {
+        private bool _InService;
+        public bool InService { 
+            get => _InService; 
+            set => InService = value;
+        }
         public int _HorsePower;
         public int _Weight;
 
@@ -14,12 +19,21 @@ namespace TrainSystem
             set
             {
                 // Add code to check if InService
+                if (! _InService)
+                {
+                    throw new Exception("Engine is not in service");
+                }
+
+                if (! Utilities.InHundreds(value))
+                {
+                    throw new Exception("Engine horsepower must be positive and non-zero");
+                }
                 /// Add code to validate value is > 0 and in 100 increments 
                 _HorsePower = value;
             }
         }
 
-        public bool InService { get; set; }
+       
         public string Model { get; private set; }
         public string SerialNumber { get; private set; }
         public int Weight
@@ -27,6 +41,16 @@ namespace TrainSystem
             get => _Weight;
             set
             {
+
+                if (!_InService)
+                {
+                    throw new Exception("Engine is not in service");
+                }
+
+                if (! Utilities.InHundreds(value))
+                {
+                    throw new Exception("Engine weight must be positive and non-zero");
+                }
                 // Add code to InService and if value is > 0 and in 100 increments
                 _Weight = value;
             }
@@ -46,17 +70,4 @@ namespace TrainSystem
 
     }
 
-    public static class Utilities
-    {
-        public static bool InHundreds(int value)
-        {
-            return value % 100 == 0;
-        }
-
-        public static bool IsPostiveNonZero(int value)
-        {
-            return value > 0;
-        }
-
-    }
 }
---
+++ TrainSystem/TrainSolution/TrainSystem/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainSystem
+{
+    public static class Utilities
+    {
+       
+        
+            public static bool InHundreds(int value)
+            {
+                return value % 100 == 0;
+            }
+
+            public static bool IsPostiveNonZero(int value)
+            {
+                return value > 0;
+            }
+
+        
+    }
+}
